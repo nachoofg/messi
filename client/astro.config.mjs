b0714ecb4c8443ec7dd353db5772f7fad41bb922
tsuchipls <jdjneke@gmail.com>
2023-09-21 15:15:25 -0300

    🎨 thath shit

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,13 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
+  output: "server",
   adapter: node({
-    mode: 'middleware',
+    mode: "middleware",
   }),
-
 });
